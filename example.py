e0bfb15d8e65c853227ff9e5ef61a1ffede59bd3
seesteins <chris@vertbag.run>
2025-11-01 11:19:18 -0400

    sync pending transactions in example

--- example.py
+++ example.py
@@ -7,6 +7,6 @@ load_dotenv()
 url = os.environ["SIMPLEFIN_URL"]
 auth = DefaultAuth.from_url(url)
 client = SimpleFinClient(auth)
-data = client.get_data(start_date=datetime.now()-timedelta(days=90), end_date=datetime.now())
+data = client.get_data(start_date=datetime.now()-timedelta(days=90), end_date=datetime.now(), pending=True)
 database = DatabaseManager()
 database.sync(data)
